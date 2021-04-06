ea5d03b9ab7b0c93d34f2b1cd93533a455371bfd
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-06 05:46:19 +0400

    Бонус за мили

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String []args) {
+        int costTikets = 4_170;
+        int bonusMiles = costTikets / 20;
+        System.out.println(bonusMiles);
+    }}
