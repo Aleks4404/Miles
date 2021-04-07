334ef35db1d6dfbf2fd5c9a194c74132486a6d2c
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-07 17:50:54 +0400

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
-    public static void main(String []args) {
+    public static void main(String[] args) {
         int costTikets = 4_170;
         int bonusMiles = 20;
         int totalMiles = costTikets / bonusMiles;
         System.out.println(totalMiles);
-    }}
+    }
+}
