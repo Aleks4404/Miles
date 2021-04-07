5bad4cc0a2e2c2f7efb3206255bfad9957eef8c0
Aleksey Efimov <Aleks4404@mail.ru>
2021-04-07 04:08:23 +0400

    update code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String []args) {
         int costTikets = 4_170;
-        int bonusMiles = costTikets / 20;
-        System.out.println(bonusMiles);
+        int bonusMiles = 20;
+        int totalMiles = costTikets / bonusMiles;
+        System.out.println(totalMiles);
     }}
